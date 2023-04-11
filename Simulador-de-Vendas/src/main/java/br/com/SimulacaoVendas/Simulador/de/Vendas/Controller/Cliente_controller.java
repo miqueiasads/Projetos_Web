8393f80e215c5b7fa87467ca8c24c2d85a794e48
Miqueias Soares Cardoso <miqueiasccccc@gmail.com>
2023-04-11 20:58:11 -0300

    Atualizações do Cliente_controller

--- Simulador-de-Vendas/src/main/java/br/com/SimulacaoVendas/Simulador/de/Vendas/Controller/Cliente_controller.java
+++ Simulador-de-Vendas/src/main/java/br/com/SimulacaoVendas/Simulador/de/Vendas/Controller/Cliente_controller.java
@@ -1,7 +1,31 @@
 package br.com.SimulacaoVendas.Simulador.de.Vendas.Controller;
 
+import br.com.SimulacaoVendas.Simulador.de.Vendas.Model.Cliente;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+
 public class Cliente_controller {
+
+    @PostMapping(value = "/novocliente")
+    public String SalvarCliente(@RequestBody Cliente cliente){
+        return null;
+    }
+    @DeleteMapping(value = "remover/{cpf}")
+    public String DeletarCliente(@PathVariable(value = "cpf")Integer cpf){
+        return null;
+        //Buscar o cliente pelo CPF e assim deleta-lo
+    }
+    @GetMapping(value = "/meusclientes")
+    public void ListarClientes(){
+
+    }
+
+    public String AtualizarCliente(){
+        return null;
+
+    }
+
 }
